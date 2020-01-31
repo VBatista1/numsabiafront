a4fd4e797a9be6c02c79c3a2adeb474062b68da7
VBatista1 <victor.batista1@hotmail.com>
2020-01-31 00:46:35 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Checkbox, Form, Container, Table } from 'semantic-ui-react';
+const axios = require("axios");
 
 function App() {
+  const [content, setContent] = useState("");
+  const [source, setSource] = useState("");
+  const [posts, setPosts] = useState("");
+  const [tableCells, setTableCells] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    }
+
+    const post = {
+      content: content,
+      source: source
+    };
+
+    setPosts(oldPosts => [...oldPosts, post]);
+    // axios.post(`http://127.0.0.1:3003/addPost`, { post }, config)
+    //   .then(res => {
+    //     console.log(res);
+    //     console.log(res.data);
+    //   });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Form.Field>
+            <label>Conteudo</label>
+            <input placeholder='Conteudo' value={content} onChange={e => setContent(e.target.value)} />
+          </Form.Field>
+          <Form.Field>
+            <label>Fonte</label>
+            <input placeholder='Fonte' value={source} onChange={e => setSource(e.target.value)} />
+          </Form.Field>
+          <Button type='submit'>Cadastrar</Button>
+        </Form>
+        
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Conteudo</Table.HeaderCell>
+              <Table.HeaderCell>Fonte</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            { posts.length > 0 ?
+              posts.map(post => (
+                <Table.Row>
+                  <Table.Cell>{post.content}</Table.Cell>
+                  <Table.Cell>{post.source}</Table.Cell>
+                </Table.Row>
+              )) : null
+            }
+          </Table.Body>
+        </Table>
+      </Container>
     </div>
   );
 }
